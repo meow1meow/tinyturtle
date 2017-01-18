c9029b4680bb1109fc2f68d95b4d63549abee93f
tabitha <lilytheboss123@gmail.com>
2017-01-18 21:55:53 +0000

    turtlemove

---
+++ script.js
@@ -0,0 +1,15 @@
+ TinyTurtle.apply(window); 
+ forward(20);
+ left(90);
+ forward(30);
+ left(90);
+ forward(20);
+ left(90);
+ forward(30);
+ left(90);
+ forward(20);
+ left(50);
+ forward(20);
+ left(80);
+ forward(18);
+ stamp(); 
